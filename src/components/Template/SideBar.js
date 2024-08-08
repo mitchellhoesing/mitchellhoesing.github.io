8c852a393b7c77892195d885450d7c2167ec1b38
mitchellhoesing <hoesingmitch02@gmail.com>
2024-08-08 12:28:04 -0700

    updating sidebar.js

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -9,48 +9,30 @@ const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+        <img src={`${PUBLIC_URL}/images/unprofessionalHeadshot256.png`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p>
-          <a href="mailto:michael@mldangelo.com">michael@mldangelo.com</a>
-        </p>
+        <h2>Mitchell Hoesing</h2>
+        <p><a href="mailto:hoesingmitch02@gmail.com">hoesingmitch02@gmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>
-        Hi, I&apos;m Michael. I am a{' '}
-        <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC
-        Alumni, and the VP of Engineering at{' '}
-        <a href="https://smileidentity.com">Smile Identity</a>. Previously, I
-        was the co-founder and CTO of <a href="https://arthena.com">Arthena</a>,
-        co-founder of <a href="https://matroid.com">Matroid</a>, and worked at{' '}
-        <a href="https://planet.com">Planet</a> and{' '}
-        <a href="https://facebook.com">Facebook</a>.
+      <p>Hi, I&apos;m Mitch. I am an alumnus of Oregon State University (OSU) and Penn State
+        University (PSU). Previously, I was a research assistant at PSU. My research performs a
+        longitudinal quantitative analysis of the CHI research conference data sources and
+        methodologies using large language models.
       </p>
       <ul className="actions">
         <li>
-          {!window.location.pathname.includes('/resume') ? (
-            <Link to="/resume" className="button">
-              Learn More
-            </Link>
-          ) : (
-            <Link to="/about" className="button">
-              About Me
-            </Link>
-          )}
+          {!window.location.pathname.includes('/resume') ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
         </li>
       </ul>
     </section>
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">
-        &copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.
-      </p>
     </section>
   </section>
 );
